a3bd8679ef78c0a4442fd7b4cdd459832b7534bd
Maddala <j-v-s-shivani.maddala@capgemini.com>
2023-10-10 23:32:28 +0530

    files added

---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,48 @@
+import {motion, AnimatePresence} from 'framer-motion'
+import PropTypes from 'prop-types'
+import FeedbackItem from './FeedbackItem'
+
+function FeedbackList({feedback, handleDelete}) {
+    if(!feedback || feedback.length === 0){
+        return <p>No Feedback Yet</p>
+    }
+
+    return (
+      <div className='feedback-list'>
+        <AnimatePresence>
+          {feedback.map((item) => (
+            <motion.div
+              key={item.id}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              layout
+            >
+              <FeedbackItem key={item.id} item={item} />
+            </motion.div>
+          ))}
+        </AnimatePresence>
+      </div>
+    )
+  
+
+  // return (
+  //   <div className="fedback-list">
+  //       {feedback.map((item) => (
+  //           <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
+  //       ))}
+  //   </div>
+  // )
+}
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        text: PropTypes.string.isRequired,
+        rating: PropTypes.number.isRequired,
+      })
+  )
+}
+
+export default FeedbackList
